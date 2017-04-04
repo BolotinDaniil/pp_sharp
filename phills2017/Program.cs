30478d9c3c38c7b04ad9fe99dbe49f17c3565433
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-04-04 23:24:27 +0300

    sync solution

--- phills2017/Program.cs
+++ phills2017/Program.cs
@@ -1,29 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+// using System.Text;
+// using System.Threading.Tasks;
+
 
 namespace phills2017
 {
     class Phills2017
     {
-        static bool debug_mode = false;
+        static bool debug_mode = true;
 
-        static int phill_count = 5;
-        static short THINK = 0;
-        static short EAT = 1;
+        const int phill_count = 5;
+        const short THINK = 0;
+        const short EAT = 1;
 
         static List<Thread> threads = new List<Thread>();
-        static List<short> phill_states = new List<short>();
-        static List<int> phill_requests = new List<int>();
-        static List<bool> free_forks = new List<bool>();
+        static List<short> phill_states = new List<short>();  // состояния философов
+        static List<int> phill_requests = new List<int>();  // количество просьб о еде от каждого философа - лист на phill_count элементов
+        static List<bool> free_forks = new List<bool>();  // статус вилок
 
-        static int slave_ttl = 500; //100
+        const int slave_ttl = 250; //100  
         static bool slave_alive = true;
+        const int slave_sleep = 5; // ms
+        const int phill_sleep = 50; // ms
 
-        //static Queue<int> queue_ = new Queue<int>(); // очередь обращений
         static object superlock = new object();
 
         static void Main(string[] args)
@@ -44,8 +46,6 @@ namespace phills2017
             Slave.Start();
             for (int i = 0; i < phill_count; i++)
                 threads[i].Start(i);
-            
-            // ...
 
             // Join all threads
             for (int i = 0; i < phill_count; i++)
@@ -54,46 +54,47 @@ namespace phills2017
 
             if (debug_mode)
             {
-                Console.WriteLine("Main end.");
+                Console.WriteLine("Main thread end.");
                 Console.ReadLine();
             }
 
         }//Main
+
+        static void intense_work()
+        {
+            double tmp = 0;
+            for (int i = 0; i < 100000000; i++)
+                tmp = (1 / tmp + i * 1.1);
+        }
 
         static void tPhill(object index)
         {
             int number = Convert.ToInt32(index);
             int left = number, 
                 right = (number + 1) % phill_count;
-            
-            double tmp = 0;
-            Random rnd = new Random();
 
             while (slave_alive) {
                 if (phill_states[number] == THINK) {
 
-                    // Thread.Sleep(5);
-                    for (int i = 0; i < 10000000; i++) // think process
-                        tmp = (1 / tmp + i * 1.1);
+                    Thread.Sleep(phill_sleep);  // ++
 
-                    lock (phill_requests)
-                        phill_requests[number]++; // ??? ask
-
-                    tmp = 0;
-                    Thread.Sleep(10);
+                    lock (superlock)  // DO REQUEST.
+                        phill_requests[number]++;
                 }
 
                 if (phill_states[number] == EAT) {
-
-                    lock (phill_requests)
-                    { // ??? to slaave
-                        //Console.WriteLine("BITCH"); // CONSOLE WRITE MTHRFKR 
+                    lock (superlock) {
+                        if (debug_mode)
+                            Console.WriteLine("phill {0} EAT end.", number); // CONSOLE WRITE MTHRFKR 
                         phill_states[number] = THINK;
                         free_forks[left] = true;
                         free_forks[right] = true;
                         phill_requests[number] = 0;
+                        
                     }
-                    Thread.Sleep(10);
+                    intense_work(); // THINK PROCESS.  // ++
+
+
                 }
             }
 
@@ -103,39 +104,30 @@ namespace phills2017
 
         static void tSlave(object index)
         {
-            int tmp = 0,
-                max_index = 0;
+            int max_index = 0;
             int left, right;
 
             for (int i = 0; i < slave_ttl; i++) {
 
-                lock (phill_requests)
+                lock (superlock)
                 {
-                    max_index = phill_requests.IndexOf(phill_requests.Max());
-                    // --
+                    max_index = phill_requests.IndexOf(phill_requests.Max()); // Выбираем самого голодного.
+
                     left = max_index;
                     right = (max_index + 1) % phill_count;
 
                     if (free_forks[left] && free_forks[right])
-                    { // ?? lock
-                        //Console.WriteLine("BITCH__{0}", max_index);
+                    {
+                        if (debug_mode)
+                            Console.WriteLine("set phill {0} to EAT", max_index);
+
                         free_forks[left] = false;
                         free_forks[right] = false;
                         phill_requests[max_index] = 0;
                         phill_states[max_index] = EAT;
                     }
-                    //--
                 }
-                Thread.Sleep(5);
-
-                if (debug_mode) {
-                    /*Console.WriteLine("___" + max_index);
-                    lock (phill_requests)
-                    {
-                        phill_requests.ForEach(Console.Write);
-                        Console.WriteLine();
-                    } */
-                }
+                Thread.Sleep(slave_sleep);
             }
                 
             if (debug_mode)
@@ -144,5 +136,5 @@ namespace phills2017
             slave_alive = false;
         }//Slave
 
-    }
+    }//class
 }
